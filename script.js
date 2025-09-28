b7d6bf828ef81b0dc217f7ace82896318649e370
Samih-iut <samih.oz@edu.univ-fcomte.fr>
2025-09-28 18:11:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,32 +6,51 @@ document.addEventListener("DOMContentLoaded", () => {
   let index = 0;
 
   function changeSlide(newIndex) {
-    // Effet fade-out
+
     heroImg.classList.remove("fade-in");
-    void heroImg.offsetWidth; // hack pour relancer l'animation CSS
+    void heroImg.offsetWidth; 
 
     index = newIndex;
     heroImg.src = images[index];
 
-    // Effet fade-in
+
     heroImg.classList.add("fade-in");
 
-    // Gestion des dots
+
     dots.forEach((dot) => dot.classList.remove("active"));
     dots[index].classList.add("active");
   }
 
-  // Auto-slide
+
   setInterval(() => {
     index = (index + 1) % images.length;
     changeSlide(index);
   }, 4000);
 
-  // Clic sur les dots
+
   dots.forEach((dot, i) => {
     dot.addEventListener("click", () => changeSlide(i));
   });
 
-  // Init
+
   changeSlide(0);
 });
+
+document.getElementById("searchBtn").addEventListener("click", () => {
+  const query = document.getElementById("searchInput").value.toLowerCase();
+
+  if (query.includes("accueil")) {
+    window.location.href = "index.html";
+  } else if (query.includes("jeux")) {
+    window.location.href = "jeux.html";
+  } else if (query.includes("histoire")) {
+    window.location.href = "histoire.html";
+  } else if (query.includes("économie") || query.includes("economie")) {
+    window.location.href = "economie.html";
+  } else if (query.includes("équipe") || query.includes("equipe")) {
+    window.location.href = "equipe.html";
+  } else {
+    alert("Page non trouvée !");
+  }
+});
+
